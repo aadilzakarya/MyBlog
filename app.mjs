6d47eacdb8ee418837d040d11139570532ccd2a1
Aadil Zakarya <az2391@nyu.edu>
2023-10-30 18:55:00 -0400

    filtering added

--- app.mjs
+++ app.mjs
@@ -1,7 +1,15 @@
+import './config.mjs';
+
 import express from 'express';
 const app = express();
 
+import './db.mjs';
+
+import mongoose from 'mongoose';
+const Review = mongoose.model('Review')
+
 // set up express static
+
 import url from 'url';
 import path from 'path';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -13,8 +21,37 @@ app.set('view engine', 'hbs');
 // body parser (req.body)
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('TODO: add / modify routes')
+
+app.get('/', async (req, res) => {
+  try {
+    // Get filtering criteria from query string
+    const { semester, year, professor } = req.query;
+
+    // Build a query object based on form input
+    const query = {};
+
+    if (semester) {
+      query.semester = semester;
+    }
+    if (year) {
+      query.year = parseInt(year);
+    }
+    if (professor) {
+      query.professor = professor;
+    }
+
+    // Use Review.find() with the query object to filter reviews or retrieve all reviews
+    const reviews = await Review.find(query);
+
+    // Render the reviews in the template
+    res.render('reviews', { reviews });
+  } catch (error) {
+    console.error('Error handling reviews:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
+
+
+
 
 app.listen(process.env.PORT || 3000);
--- db.mjs
+++ db.mjs
@@ -1,11 +1,25 @@
 // db.mjs
 import mongoose from 'mongoose';
-
-// Ensure that you have previously completed the configuration steps
-// (i.e., you have a .env file, config.mjs, and the appropriate import statements).
+import {config} from 'dotenv';
 
 // Uncomment the following line to debug the value of the database connection string.
-// console.log(process.env.DSN);
+
+config();
+
+console.log(process.env.DSN);
+
+// Define your Mongoose schema for the reviews
+const reviewSchema = new mongoose.Schema({
+  courseNumber: { type: String, required: true },
+  courseName: { type: String, required: true },
+  semester: { type: String, required: true },
+  year: { type: Number, required: true },
+  professor: { type: String, required: true },
+  review: { type: String, required: true },
+});
+
+// Create a Mongoose model using the schema
+const Review = mongoose.model('Review', reviewSchema);
 
 // Define any Mongoose connection options here.
 const mongooseOpts = {
@@ -13,14 +27,15 @@ const mongooseOpts = {
   useUnifiedTopology: true,
 };
 
-// Connect to the MongoDB database using the DSN from your .env file.
-mongoose.connect(process.env.DSN, mongooseOpts, (err) => {
-  if (err) {
-    console.error('Failed to connect to the database:', err);
-  } else {
+// // Connect to the MongoDB database using the DSN from your .env file.
+mongoose
+  .connect(process.env.DSN, mongooseOpts)
+  .then(() => {
     console.log('Connected to the database');
-  }
-});
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database:', err);
+  });
 
-// Export the Mongoose instance for use in other parts of your application.
-export default mongoose;
+// Export the Mongoose instance and the Review model for use in other parts of your application.
+export { mongoose, Review };
