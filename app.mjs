dc9d5a5ac9d4d180ba2bd17fe75153b5230ef61b
Aadil Zakarya <az2391@nyu.edu>
2023-10-30 20:38:34 -0400

    count added

--- app.mjs
+++ app.mjs
@@ -1,6 +1,7 @@
 import './config.mjs';
 
 import express from 'express';
+import session from 'express-session';
 const app = express();
 
 import './db.mjs';
@@ -20,6 +21,27 @@ app.set('view engine', 'hbs');
 
 // body parser (req.body)
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: 'your-secret-key', // Change this to a  random secret
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Middleware to track page visits
+app.use((req, res, next) => {
+  req.session.visitCount = req.session.visitCount ? req.session.visitCount + 1 : 1;
+  res.locals.visitCount = req.session.visitCount;
+  console.log("COUNT: ", res.locals.visitCount)
+  app.locals.count = res.locals.visitCount; 
+
+  next();
+});
+
+
+
 
 
 app.get('/', async (req, res) => {
@@ -50,6 +72,39 @@ app.get('/', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+// GET handler for showing the form
+app.get('/reviews/add', (req, res) => {
+  res.render('add-review'); // Render the form template
+});
+
+// POST handler for processing form submissions
+app.post('/reviews/add', async (req, res) => {
+  try {
+    // Extract review data from req.body
+    const { courseNumber, courseName, semester, year, professor, review } = req.body;
+
+    // Create a new review document based on the data
+    const newReview = new Review({
+      courseNumber,
+      courseName,
+      semester,
+      year,
+      professor,
+      review,
+    });
+
+    // Save the new review to the database
+    await newReview.save();
+
+    // Redirect back to the page that displays all reviews
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error adding a review:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 
 
 
